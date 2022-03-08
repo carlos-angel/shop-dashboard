3118acd4a31bb5c4930a2ab58249c3d3e30b3732
carlos-angel <c.a.a.a.503@gmail.com>
2022-03-08 08:08:50 -0600

    feat: agregar MainLayout

---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,14 @@
+import Header from "@components/Header";
+import Nav from "@common/Nav";
+
+export default function MainLayout({ children }) {
+  return (
+    <div className="min-h-full">
+      <Header />
+      <Nav />
+      <main>
+        <div className="py-6 mx-auto max-w-7xl sm:px-6 lg:px-8">{children}</div>
+      </main>
+    </div>
+  );
+}
