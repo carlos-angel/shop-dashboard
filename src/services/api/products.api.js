082ed973bf36e007ec5a12c0a11566976eba067e
carlos-angel <c.a.a.a.503@gmail.com>
2022-03-20 09:23:38 -0600

    feat: agregar servicio para editar un producto

--- src/services/api/products.api.js
+++ src/services/api/products.api.js
@@ -16,9 +16,33 @@ export const addProduct = async (body) => {
   }
 };
 
+export const editProduct = async ({ id, body }) => {
+  try {
+    const config = {
+      headers: {
+        accept: "*/*",
+        "Content-Type": "application/json",
+      },
+    };
+    const response = await axios.put(endpoints.products.updateProduct(id), body, config);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 export const deleteProduct = async (id) => {
   try {
     const response = await axios.delete(endpoints.products.deleteProduct(id));
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getProduct = async (id) => {
+  try {
+    const response = await axios.get(endpoints.products.getProduct(id));
     return response.data;
   } catch (error) {
     console.log(error);
