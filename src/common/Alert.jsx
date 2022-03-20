c53683c54f4fd83d2c9e4bc917e91d74fceb4b9a
carlos-angel <c.a.a.a.503@gmail.com>
2022-03-19 23:47:34 -0600

    refactor: agregar configuración de estilos del Alert

--- src/common/Alert.jsx
+++ src/common/Alert.jsx
@@ -1,38 +1,19 @@
 import { XCircleIcon } from "@heroicons/react/outline";
 import React from "react";
+import config from "@utils/alert-styles-config.util";
 
 export default function Alert({ type, message, onClose }) {
-  let colors = "";
-  let svgColor = "";
-  let typeMessage = "";
-  switch (type) {
-    case "danger":
-      colors = "border-red-400 text-red-700 bg-red-100";
-      svgColor = "text-red-500";
-      typeMessage = "Error! ";
-      break;
-
-    case "success":
-      colors = "border-green-400 text-green-700 bg-green-100";
-      svgColor = "text-green-500";
-      typeMessage = "Success! ";
-      break;
-
-    default:
-      break;
-  }
-
   return (
     <div
-      className={`flex items-center justify-between border px-4 py-3 rounded relative mb-5 ${colors}`}
+      className={`flex items-center justify-between border px-4 py-3 rounded relative mb-5 ${config[type].colorsContent}`}
       role="alert"
     >
       <div>
-        <strong className="font-bold">{typeMessage}</strong>
+        <strong className="font-bold">{config[type].titleMessage}</strong>
         <span className="block sm:inline">{message}</span>
       </div>
       <button type="button">
-        <XCircleIcon className={`w-6 h-6 ${svgColor}`} onClick={onClose} />
+        <XCircleIcon className={`w-6 h-6 ${config[type].colorIcon}`} onClick={onClose} />
       </button>
     </div>
   );
---
+++ src/utils/alert-styles-config.util.js
@@ -0,0 +1,14 @@
+const config = {
+  success: {
+    colorsContent: "border-green-400 text-green-700 bg-green-100",
+    colorIcon: "text-green-500",
+    titleMessage: "Success! ",
+  },
+  danger: {
+    colorsContent: "border-red-400 text-red-700 bg-red-100",
+    colorIcon: "text-red-500",
+    titleMessage: "Error! ",
+  },
+};
+
+export default config;
